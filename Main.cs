747e250e018f0c36da2fa1cf926c56be03d656dc
Kerrigan <kavex1988@gmail.com>
2022-03-15 20:04:50 -0600

    1.0.0

---
+++ Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TP
+{
+
+    public class Main : MelonMod
+    {
+        GameObject quickMenu;
+        GameObject quickInterfaceMenu;
+        public static Transform playlistButton =>
+        GameObject.Find("UserInterface/MenuContent/Screens/UserInfo/Buttons/RightSideButtons/RightUpperButtonColumn/PlaylistsButton").transform;
+
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(QMInitializer());
+        }
+
+        private IEnumerator QMInitializer()
+        {
+            while ((quickMenu = GameObject.Find("UserInterface/Canvas_QuickMenu(Clone)")) == null)
+                yield return null;
+            while ((quickInterfaceMenu = GameObject.Find("UserInterface/MenuContent/")) == null)
+                yield return null;
+            SelMenu();
+        }
+        public static Button CreateUserInfoButton(string buttonText, System.Action buttonAction)
+        {
+            Button button = GameObject.Instantiate(playlistButton, playlistButton.parent, true).GetComponent<Button>();
+            Text btnText = button.GetComponentInChildren<Text>();
+
+            btnText.text = buttonText;
+            button.onClick = new Button.ButtonClickedEvent();
+            button.onClick.AddListener(buttonAction);
+
+            return button;
+        }
+
+        private void SelMenu()
+        {
+            Transform quickTP = quickMenu.transform.Find("Container/Window/QMParent/Menu_SelectedUser_Local/ScrollRect/Viewport/VerticalLayoutGroup/Buttons_UserActions");
+
+            Utils.CreateDefaultButton("Teleport", new Vector3(0, -25, 0), "Teleport to selected player", Color.white, quickTP,
+                new Action(() => {
+                    Utils.LocalPlayer.gameObject.transform.position = Utils.MenuControl.activePlayer.gameObject.transform.position;
+                }));
+
+            Button tpButton = CreateUserInfoButton("Teleport", () =>
+            {
+                Utils.LocalPlayer.gameObject.transform.position = Utils.MenuControl.activePlayer.gameObject.transform.position;
+            });
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle("Teleport to the selected player")]
+[assembly: AssemblyDescription("Teleport to the selected player")]
+[assembly: AssemblyProduct("TP")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: MelonInfo(typeof(TP.Main), "TP", "1.0.0", "Kavex")]
+[assembly: MelonGame("VRChat", "VRChat")]
